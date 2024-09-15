a35268a8c67ceee093bb15d8e1b250673148c663
Nathaniyell <essien.nathan@yahoo.com>
2024-09-15 03:00:46 +0100

    add cards

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue"
+import Hero from "@/components/Hero.vue"
+import HomeCards from "@/components/HomeCards.vue"
 
 </script>
 
 <template>
  <Navbar />
+ <Hero title="Test Title" subTitle="Find vue jobs" />
 </template>
+
+<HomeCards />
 
 <style scoped>
 
