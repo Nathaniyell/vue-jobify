959a2732caaecfa0d0e227cf5002b65aad40e2ef
Nathaniyell <essien.nathan@yahoo.com>
2024-09-17 01:53:24 +0100

    use RouterLink to route between pages

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,12 @@
 <script setup>
+import {RouterLink, useRoute} from "vue-router"
 
-// const
 
+
+const isActiveLink = (routePath) =>{
+    const route = useRoute()
+    return route.path === routePath
+}
 </script>
 
 
@@ -11,17 +16,17 @@
       <div class="flex h-20 items-center justify-between">
         <div class="flex flex-1 items-center justify-between md:items-stretch md:justify-between">
           <!-- Logo -->
-          <a class="block text-white text-2xl font-bold ml-2" href="/">
+          <RouterLink class="block text-white text-2xl font-bold ml-2" to="/">
 
             Vue JOBify
 
-          </a>
+          </RouterLink>
           <div class="md:ml-auto">
             <div class="flex space-x-2">
-              <a href="/"
-                class="text-white bg-violet-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</a>
-              <a href="/jobs" class="text-white hover:bg-violet-900 hover:text-white rounded-md px-3 py-2">Jobs</a>
-              <a href="/jobs/add" class="text-white hover:bg-violet-900 hover:text-white rounded-md px-3 py-2">Add Job</a>
+              <RouterLink to="/"
+                :class="[isActiveLink('/')? 'bg-violet-900': 'hover:bg-gray-900 hover:text-white','text-white', 'py-2', 'px-3', 'rounded-md']">Home</RouterLink>
+              <RouterLink to="/jobs" :class="[isActiveLink('/jobs')? 'bg-violet-900': 'hover:bg-gray-900 hover:text-white','text-white', 'py-2', 'px-3', 'rounded-md']">Jobs</RouterLink>
+              <RouterLink to="/jobs/add" :class="[isActiveLink('/jobs/add')? 'bg-violet-900': 'hover:bg-gray-900 hover:text-white','text-white', 'py-2', 'px-3', 'rounded-md']">Add Job</RouterLink>
             </div>
           </div>
         </div>
