604672a2fc95295a51463d8b742a00016509b176
Nathaniyell <essien.nathan@yahoo.com>
2024-09-17 05:39:22 +0100

    update Hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -21,18 +21,18 @@ defineProps({
 </script>
 
 <template>
-
-    <!-- Hero -->
-    <section class="bg-violet-700 py-20 mb-4">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
-            <div class="text-center">
-                <h1 class="text-4xl font-bold text-white sm:text-5xl md:text-6xl">
-                   {{title}}
-                </h1>
-                <p class="my-4 text-xl text-white">
-                  {{subTitle}}
-                </p>
-            </div>
+    <section class="relative bg-cover bg-center h-64 lg:h-screen grid place-items-center mb-4">
+    <div class="absolute inset-0 bg-[url('../assets/images/vue4.jpg')] bg-cover bg-center filter brightness-50"></div>
+    <div class="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold text-white sm:text-5xl md:text-6xl">
+                {{title}}
+            </h1>
+            <p class="my-4 text-xl text-white">
+                {{subTitle}}
+            </p>
         </div>
-    </section>
+    </div>
+</section>
+
 </template>
